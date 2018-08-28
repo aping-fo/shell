ce01aeacf8116260aa4942e4136a87096fde9d0e
aping.fo <125455364@qq.com>
2018-08-28 09:46:14 +0800

    Update reads.sh

--- reads.sh
+++ reads.sh
@@ -6,3 +6,27 @@ then
 else
   echo "sorry"
 exit 0
+
+# 控制输入字符长度 -nNumber Number表示控制字符的长度,超过则read命令立即接受输入并将其传给变量。无需按回车键。
+read -n1 -p "do you agree with me [Y/N]" ans
+case $ans in
+Y/y )
+  echo "Y"
+N|n)
+  echo "N"
+*)
+  echo "other"
+esac;
+
+# 读取文件,通过cat file 配合管道处理,如 cat file | while read line
+count=1
+if read -p "choice ou file:" yourfile; then
+  cat $yourfile | while read line
+  do
+    echo "line $count:$line"
+    let count++
+  done 
+  exit 0
+else
+  echo "none"
+  exit 0
