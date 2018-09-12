1a4b2579485e9c2ab3c7a10f6c5329a27dbb0ed4
aping.fo <125455364@qq.com>
2018-09-12 19:50:11 +0800

    新增一种计算方式

--- longest_non_repeat.py
+++ longest_non_repeat.py
@@ -34,4 +34,20 @@ def longest_non_repeat_v1(str1):
     print(str1[max_begin_idx:max_end_idx])
 
 
+def longest_non_repeat_v2(string):
+    """
+    Apply slide windown.
+    """
+    dict = {}  # 记录每个字符最后出现位置
+    max_length = 0
+    j = 0
+    for i in range(len(string)):
+        if string[i] in dict:
+            j = max(dict[string[i]], j)  # 最后一个重复出现的字符位置
+        dict[string[i]] = i + 1
+        max_length = max(max_length, i - j + 1)
+    print(max_length)
+
+
 longest_non_repeat_v1('abcabcbbcdef')
+longest_non_repeat_v2('abcabcbbcdef')
