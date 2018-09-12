69313e77cafc2e97c488688293c42e80524bb709
aping.fo <125455364@qq.com>
2018-09-12 14:15:55 +0800

    Update delete_nth.py

--- delete_nth.py
+++ delete_nth.py
@@ -1,10 +1,21 @@
 import collections
 
 '''
+从一个数组中，选择出现小于等于目标次数的元素
 
+列如：
+input array = [1,2,3,1,2,1,2,3]，元素可重复次数是2
+output result = [1,2,3,1,2,3]
 '''
 
+
 def delete_nth_native(array, n):
+    '''
+
+    :param array: 目标数组
+    :param n: 元素可重复次数
+    :return: 返回一个结果数组
+    '''
     ans = []
     for num in array:
         if ans.count(num) < n:
