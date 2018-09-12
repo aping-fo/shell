da3d3dd0303fbd08aeb63345d209e9b8e31fb25c
aping.fo <125455364@qq.com>
2018-09-12 17:03:37 +0800

    新增一种方式

--- josephus.py
+++ josephus.py
@@ -22,5 +22,18 @@ def josephus(int_list, skip):
             skiptmp -= 1
 
 
+def josephus1(int_list, skip):
+    skip = skip - 1
+    idx = 0
+    len_list = len(int_list)
+    while len_list > 0:
+        idx = (skip + idx) % len_list
+        yield int_list.pop(idx)
+        len_list -= 1
+
+
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 josephus(array, 3)
+
+array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+josephus1(array1, 3)
