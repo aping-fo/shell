dfd32c50ebc95c217ab02bf72f24456d4b354a4f
aping.fo <125455364@qq.com>
2018-09-12 14:32:17 +0800

    learn python two
    learn python two

--- flatmap.py
+++ flatmap.py
@@ -7,7 +7,7 @@ import collections
 
 def flatmap(array, output_arr=None):
     '''
-
+        递归方式进行展开
     :param array: 输入数组
     :param output_arr: 传出数组
     :return:
@@ -22,8 +22,26 @@ def flatmap(array, output_arr=None):
             output_arr.append(ele)
 
 
+'''
+学习利用yield from 和 yield
+'''
+
+
+def flatmap_iter(iterable):
+    for ele in iterable:
+        if isinstance(ele, collections.Iterable):
+            yield from flatmap_iter(ele)
+        else:
+            yield ele
+
+
 array = [1, 2, 3, [4, 5, 6], 7, 8]
 reslut = []
 
 flatmap(array, output_arr=reslut)
 print(reslut)
+
+r = flatmap_iter(array.__iter__())
+
+for i in r:
+    print(i)
