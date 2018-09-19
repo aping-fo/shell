292a41c10941470fe306965ba0bda52558aa844b
aping.fo <125455364@qq.com>
2018-09-19 11:47:57 +0800

    Update combination_sum.py

--- combination_sum.py
+++ combination_sum.py
@@ -8,22 +8,21 @@
 
 import itertools
 
-
 """
 利用Python 的API来计算
 (组合可以重复)
 
 """
+
+
 def combination_sum1(arr, target):
     res = []
     lenght = len(arr)
     for size in range(1, lenght):
-        for i in itertools.product(arr, repeat=size):  #全排列，可以重组自己
+        for i in itertools.combinations_with_replacement(arr, size):  # 全排列，可以重组自己
             s = sum(i)
             if s == target:
-                i = sorted(i)
-                if i not in res:
-                    res.append(i)
+                res.append(list(i))
     return res
 
 
@@ -32,11 +31,13 @@ def combination_sum1(arr, target):
 (组合)
 
 """
+
+
 def combination_sum2(arr, target):
     res = []
     lenght = len(arr)
     for size in range(1, lenght):
-        for i in itertools.combinations(arr, size): # 组合，不可重组自己
+        for i in itertools.combinations(arr, size):  # 组合，不可重组自己
             s = sum(i)
             if s == target:
                 i = sorted(i)
@@ -44,9 +45,9 @@ def combination_sum2(arr, target):
                     res.append(i)
     return res
 
+
 arr = [2, 3, 6, 7]
 res = combination_sum1(arr, 7)
 res1 = combination_sum2(arr, 7)
 print(res1)
 print(res)
-
