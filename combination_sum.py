de2ef9847e4b7437a9f120e1db2475962ec90ec7
aping.fo <125455364@qq.com>
2018-09-19 11:39:09 +0800

    利用python进行排列组合求解问题

---
+++ combination_sum.py
@@ -0,0 +1,52 @@
+'''
+
+给出指定数组 和指定 目标值
+
+求出所有组合和等于目标值
+
+'''
+
+import itertools
+
+
+"""
+利用Python 的API来计算
+(组合可以重复)
+
+"""
+def combination_sum1(arr, target):
+    res = []
+    lenght = len(arr)
+    for size in range(1, lenght):
+        for i in itertools.product(arr, repeat=size):  #全排列，可以重组自己
+            s = sum(i)
+            if s == target:
+                i = sorted(i)
+                if i not in res:
+                    res.append(i)
+    return res
+
+
+"""
+利用Python 的API来计算
+(组合)
+
+"""
+def combination_sum2(arr, target):
+    res = []
+    lenght = len(arr)
+    for size in range(1, lenght):
+        for i in itertools.combinations(arr, size): # 组合，不可重组自己
+            s = sum(i)
+            if s == target:
+                i = sorted(i)
+                if i not in res:
+                    res.append(i)
+    return res
+
+arr = [2, 3, 6, 7]
+res = combination_sum1(arr, 7)
+res1 = combination_sum2(arr, 7)
+print(res1)
+print(res)
+
