69c861db133347953311bbac2d9f4199a6ffe70c
aping.fo <125455364@qq.com>
2018-09-21 11:20:50 +0800

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -11,6 +11,24 @@ def subsets_v1(nums):
     return res
 
 
+def subsets_v2(nums):
+    def backtrack(res, nums, stack, pos):
+        if pos == len(nums):
+            res.add(tuple(stack))
+        else:
+            stack.append(nums[pos])
+            backtrack(res, nums, stack, pos + 1)
+            stack.pop()
+            backtrack(res, nums, stack, pos + 1)
+
+    res = set()
+    backtrack(res, nums, [], 0)
+    print(list(res))
+
+
 test = [1, 2, 3]
 res = subsets_v1(test)
 print(res)
+
+test1 = [1,2]
+subsets_v2(test1)
