95095ca679d047a8cc2de9eddd6bb9486ccb51ab
aping.fo <125455364@qq.com>
2018-09-29 18:05:01 +0800

    二分查找，递归实现

--- binary_search.py
+++ binary_search.py
@@ -14,7 +14,22 @@ def binary_search(array, query):
     return None
 
 
+def binary_search_recur(array, query, low, high):
+    if low > high:
+        return -1
+    mid = (low + high) // 2
+    midVal = array[mid]
+    if query == midVal:
+        return mid
+    elif query > midVal:
+        return binary_search_recur(array, query, mid + 1, high)
+    else:
+        return binary_search_recur(array, query, low, mid - 1)
+
+
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 ret = binary_search(arr, 10)
 
+print(ret)
+ret = binary_search_recur(arr, 10, 0, 9)
 print(ret)
