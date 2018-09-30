ed332feabea8c74b55340a173a7296b665668b9e
aping.fo <125455364@qq.com>
2018-09-30 10:15:15 +0800

    learn111

--- find_min_rotate.py
+++ find_min_rotate.py
@@ -11,5 +11,16 @@ def find_min_rotate(array):
     return array[low]
 
 
+def find_min_rotate_recur(array, low, high):
+    mid = (low + high) // 2
+
+    if mid == high:
+        return array[mid]
+    elif array[mid] > array[high]:
+        return find_min_rotate_recur(array, mid + 1, high)
+    else:
+        return find_min_rotate_recur(array, low, mid - 1)
+
+
 arr = [4, 5, 6, 7, 8, 0, 1, 2, 3]
 print(find_min_rotate(arr))
