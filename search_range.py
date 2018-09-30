66c284bb9bb134323cc3853010df45d66976ec25
aping.fo <125455364@qq.com>
2018-09-30 14:30:44 +0800

    learn41，二分法查找区间

--- search_range.py
+++ search_range.py
@@ -11,5 +11,23 @@ def search_range(array, target):
     print(start, end)
 
 
-nums = [5, 7, 7, 8, 8, 8, 10]
-search_range(nums, 11)
+def binary_search_range(array, query):
+    lo = 0
+    hi = len(array) - 1
+    while lo <= hi:
+        mid = (hi + lo) // 2
+        if array[mid] == query and array[mid - 1] < query:
+            break
+        elif array[mid] < query:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+
+    for i in range(len(array) - 1, -1, -1):
+        if query == array[i]:
+            return (mid, i)
+
+
+nums = [1, 5, 7, 7, 7, 8, 8, 8, 8, 10]
+search_range(nums, 8)
+print(binary_search_range(nums, 8))
